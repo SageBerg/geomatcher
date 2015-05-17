9bb0fd35c4f9a4a05666ac7af9f298591690d87e
Sage Berg <sberg@uoregon.edu>
2015-05-16 18:55:20 -0700

    continued working on the style and played around with some js that might be useful

--- app/client/app.js
+++ app/client/app.js
@@ -23,4 +23,25 @@ var main = function (){
     });
 }
 
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+var score = 0;
+
+function inc_score(ev) {
+    score += 1;
+    document.getElementById("current_score").innerHTML = score;
+}
+
 $(document).ready(main);
